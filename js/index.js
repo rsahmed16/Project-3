cc7c92062639bcb3c932921dec356fc531e3a69a
rsahmed16 <112486837+rsahmed16@users.noreply.github.com>
2022-11-22 23:21:52 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,8 @@
 function initMap() {
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 14.675796515578558, lng: 77.59489702704062},,
-    zoom: 12,
-	zoomControl: false,
-    scaleControl: true,
-	mapTypeId: 'hybrid'
-  });
-  
-  var marker = new google.maps.Marker({
-    position: { lat: 14.675796515578558, lng: 77.59489702704062},,
-    map: map,
-  });
-  
-  var infowindow = new google.maps.InfoWindow({
-    content:"That's my University"
-  });
-  infowindow.open(map, marker);
-}
-window.initMap = initMap;
+    var map = new google.maps.Map(document.getElementById("map"), {zoom: 20, center: {lat:41.900626660924985, lng:-87.62586197929068}, });
+    var marker = new google.maps.Marker({position: {lat:41.900626660924985, lng:-87.62586197929068}, map: map,});
+    var infowindow = new google.maps.InfoWindow({
+      content:"I love this store" });
+    infowindow.open(map, marker);
+  }
+  window.initMap = initMap;
